f4a9ad4190c065637bdadd6476eb977e490307cc
miko <miko3k@gmail.com>
2019-01-29 03:08:16 +0100

    added INTERNAL_ERROR

--- src/main/java/org/deletethis/search/parser/ErrorCode.java
+++ src/main/java/org/deletethis/search/parser/ErrorCode.java
@@ -5,5 +5,10 @@ public enum ErrorCode {
     BAD_SYNTAX,
     USAGE_NOT_ALLOWED,
     NO_URL,
-    INVALID_METHOD
+    INVALID_METHOD,
+    /**
+     * Currently not used, but will be kept, may be useful for subclasses to indicate
+     * some application level problem
+     */
+    @SuppressWarnings("unused") INTERNAL_ERROR
 }
